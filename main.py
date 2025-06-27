1714eaa8cdd59211947eedbd7f6fccea1ded17e8
lukenda71 <inarrestabilidigitali@gmail.com>
2025-06-27 13:07:45 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,42 +1,17 @@
-from flask import Flask, render_template, request, redirect, url_for
-import uuid
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Dizionario per memorizzare i dati temporaneamente
-user_data = {}
-
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    nome_promoter = request.form.get('nome_promoter')
-    cognome_promoter = request.form.get('cognome_promoter')
-    nome_ospite = request.form.get('nome_ospite')
-    regione_ospite = request.form.get('regione_ospite')
-
-    # Genera ID univoco breve
-    unique_id = str(uuid.uuid4())[:8]
-
-    # Salva dati
-    user_data[unique_id] = {
-        'nome_promoter': nome_promoter,
-        'cognome_promoter': cognome_promoter,
-        'nome_ospite': nome_ospite,
-        'regione_ospite': regione_ospite
-    }
-
-    return redirect(url_for('video', uid=unique_id))
-
-@app.route('/video/<uid>')
-def video(uid):
-    if uid in user_data:
-        dati = user_data[uid]
-        return render_template('video.html', **dati)
-    else:
-        return "Link non valido o scaduto", 404
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/video', methods=['POST'])
+def video():
+    nome_promoter = request.form['nome_promoter']
+    cognome_promoter = request.form['cognome_promoter']
+    nome_ospite = request.form['nome_ospite']
+    return render_template("video.html",
+                           nome_promoter=nome_promoter,
+                           cognome_promoter=cognome_promoter,
+                           nome_ospite=nome_ospite)
